daaae7c3976f7a91aa23936e1c3cfbaaf33f087a
Toxic254 <cyrus_g@outlook.com>
2021-05-26 20:46:48 +0300

    Code Refactor.

--- main.js
+++ main.js
@@ -44,13 +44,13 @@ scene.add(pointLight, ambientLight);
 // const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const geometry = new THREE.ConeGeometry(0.25, 1, 24);
   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
   const star = new THREE.Mesh(geometry, material);
 
   const [x, y, z] = Array(3)
     .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
+    .map(() => THREE.MathUtils.randFloatSpread(200));
 
   star.position.set(x, y, z);
   scene.add(star);
@@ -65,18 +65,33 @@ scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load('jeff.png');
+const cyrusTexture = new THREE.TextureLoader().load('cyrus.jpeg');
 
-const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
+const cyrus = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: cyrusTexture }));
 
-scene.add(jeff);
+scene.add(cyrus);
 
-// Moon
+// Sun
 
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const sunTexture = new THREE.TextureLoader().load('sun.png');
+const normTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const sun = new THREE.Mesh(
+  new THREE.SphereGeometry(4.5, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: sunTexture,
+    normalMap: normTexture,
+  })
+);
+
+scene.add(sun);
+
+// Earth
+
+const moonTexture = new THREE.TextureLoader().load('earth.jpg');
 const normalTexture = new THREE.TextureLoader().load('normal.jpg');
 
-const moon = new THREE.Mesh(
+const earth = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
   new THREE.MeshStandardMaterial({
     map: moonTexture,
@@ -84,24 +99,31 @@ const moon = new THREE.Mesh(
   })
 );
 
-scene.add(moon);
+scene.add(earth);
 
-moon.position.z = 30;
-moon.position.setX(-10);
+earth.position.z = 20;
+earth.position.setX(-10);
 
-jeff.position.z = -5;
-jeff.position.x = 2;
+sun.position.z = 30;
+sun.position.setX(-10);
+
+cyrus.position.z = -5;
+cyrus.position.x = 2;
 
 // Scroll Animation
 
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
-  moon.rotation.x += 0.05;
-  moon.rotation.y += 0.075;
-  moon.rotation.z += 0.05;
+  earth.rotation.x += 0.005;
+  earth.rotation.y += 0.075;
+  earth.rotation.z += 0.005;
 
-  jeff.rotation.y += 0.01;
-  jeff.rotation.z += 0.01;
+  sun.rotation.x += 0.05;
+  sun.rotation.y += 0.075;
+  sun.rotation.z += 0.05;
+
+  cyrus.rotation.y += 0.01;
+  cyrus.rotation.z += 0.01;
 
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
@@ -120,7 +142,7 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
-  moon.rotation.x += 0.005;
+  sun.rotation.x += 0.005;
 
   // controls.update();
 
